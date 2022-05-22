995c3da50f29eff883cefcdff1772c4021f22fa9
Moria Mizrachi <moria2115@gmail.com>
2022-05-22 15:17:45 +0300

    first commit

---
+++ config/session.js
@@ -0,0 +1,31 @@
+const expressSession = require("express-session");
+const mongodbStore = require("connect-mongodb-session");
+
+function createSessionStore() {
+  const MongoDBStore = mongodbStore(expressSession);
+
+  const store = new MongoDBStore({
+    uri: "mongodb://127.0.0.1:27017",
+    databaseName: "online-shop",
+    collection: "sessions",
+  });
+
+  return store;
+}
+
+function createSessionConfig(store) {
+  return {
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: createSessionStore(),
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    },
+  };
+}
+
+module.exports = {
+  createSessionStore: createSessionStore,
+  createSessionConfig: createSessionConfig,
+};
---
+++ middlewares/check-auth.js
@@ -0,0 +1,15 @@
+async function chaeckAuthStatus(req, res, next) {
+  const uid = req.session.uid;
+
+  if (!uid) {
+    return next();
+  }
+
+  res.locals.uid = uid;
+  res.locals.isAuth = true;
+  res.locals.isAdmin = req.session.isAdmin;
+
+  next();
+}
+
+module.exports = chaeckAuthStatus;
